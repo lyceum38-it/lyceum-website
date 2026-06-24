cce91a2a3170a7eab59b01e77e2f67f14447082c
lyceum38-it <poltava.school38@gmail.com>
2026-06-24 20:05:05 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Відкриття та закриття бокового меню
+function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('active');
+    document.getElementById('sidebarOverlay').classList.toggle('active');
+}
 
+// Відкриття та закриття підменю (Акордеон)
+function toggleSub(event) {
+    event.preventDefault(); 
+    let parentLi = event.target.closest('.has-children');
+    parentLi.classList.toggle('open');
+}
